3f40d881482c4855201e8b67d05f9c963a8844b4
unknown <jonathanbonatto.pf020@academico.ifsul.edu.br>
2025-10-23 21:01:02 -0300

    updated files

---
+++ src/model/Vendedor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author 20241PF.CC0020
+ */
+public class Vendedor extends Pessoa {
+    private double salario;
+    private double comissao;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+}
